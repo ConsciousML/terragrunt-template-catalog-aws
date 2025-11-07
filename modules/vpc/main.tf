5202a83983a893705a13dc39609cfc40882dcf68
Axel Mendoza <axel.mendoza@epita.fr>
2025-11-07 18:37:39 +0100

    update(modules/vpc): to AWS

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,31 +1,5 @@
-resource "google_compute_network" "vpc" {
-  name                    = var.network_name
-  auto_create_subnetworks = false
-}
-
-#tfsec:ignore:google-compute-enable-vpc-flow-logs
-resource "google_compute_subnetwork" "subnet" {
-  name          = var.subnet_name
-  ip_cidr_range = var.subnet_cidr
-  region        = var.region
-  network       = google_compute_network.vpc.id
-}
-
-# Reserve IP range for private services
-resource "google_compute_global_address" "private_service_range" {
-  name          = "${var.network_name}-private-services"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 16
-  network       = google_compute_network.vpc.id
-}
-
-# Create private service connection
-# Required to allow Google services such as CloudSQL to use the VPC
-resource "google_service_networking_connection" "private_vpc_connection" {
-  network                 = google_compute_network.vpc.id
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_service_range.name]
-
-  deletion_policy = "ABANDON"
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+  enable_dns_support = var.enable_dns_support
+  tags = var.tags
 }
