3433ebf6d6f6bedd19464ea9be1b7f69a8973607
Axel Mendoza <axel.mendoza@epita.fr>
2025-11-12 20:16:17 +0100

    remove(iam_role_gh_actions): :ref:refs/heads/ for matching all branches

--- modules/iam_role_github_actions/main.tf
+++ modules/iam_role_github_actions/main.tf
@@ -15,7 +15,7 @@ resource "aws_iam_role" "github_actions" {
         Condition = {
           StringLike = {
             #"token.actions.githubusercontent.com:sub" = "repo:${var.github_username}/${var.github_repo_name}:ref:refs/heads/${var.github_branch}"
-            "token.actions.githubusercontent.com:sub" : "repo:${var.github_username}/${var.github_repo_name}:ref:refs/heads/${var.github_branch}"
+            "token.actions.githubusercontent.com:sub" : "repo:${var.github_username}/${var.github_repo_name}:${var.github_branch}"
           }
           StringEquals = {
             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
